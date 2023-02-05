b6dccd279e53653bd7a9bf3881540d142c78e07d
santunu <santanuroy253396@gmail.com>
2023-02-05 20:59:39 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,13 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [txt,setText]= useState("");
+  const [id,setId]=useState("click");
   return (
     <div id="main">
-      // Do not alter the main div
+     <button id={id}
+     onClick={()=>{setText("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");setId("para")}}>click</button>
+  <p>{txt}</p>
     </div>
   );
 }
